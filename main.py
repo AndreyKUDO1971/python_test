77eabb4cf5f030bbd1f0b2f48ea5bfce352a78a9
Андрей <a89204341234@gmail.com>
2022-03-04 21:58:10 +0300

    мой первый калькулятор

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Привет")
+print("Введите число")
+x = int(input())
+print("Введите второе число")
+y = int(input())
+
+
+print("ввудите операцию")
+z = input()
+print("результат равен")
+# В зависимости от знака будем выполнять разные действия.
+if z == "*":
+    print(x * y)
+if z == "/" or z == ":":
+    print(x / y)
+if z == "+":
+    print(x + y)
+if z == "-":
+    print(x - y)
+if z == "^" or z == "**":
+    print(x ** y)
+# print()
